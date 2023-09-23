5ffdb0fd17764eb3758472d1e4be68f1776fcd2b
methodDen <daniyarabsatov@gmail.com>
2023-09-23 23:32:19 +0600

    finished

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from fastapi.responses import ORJSONResponse
 from src.core.config import app_settings
 
 app = FastAPI(
-    title=app_settings.project_name,
+    title=app_settings.app_title,
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
