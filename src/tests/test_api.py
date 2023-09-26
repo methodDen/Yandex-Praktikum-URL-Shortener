83f0b11f337e434f8f97394d693e4db92f77cc39
dani <daniyarabsatov@gmail.com>
2023-09-26 21:55:23 +0600

    added tests

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -50,24 +50,3 @@ async def test_create_short_url_batch(
     assert json[0]['original_url'] == payload[0]['original_url']
     assert json[1]['original_url'] == payload[1]['original_url']
 
-
-@pytest.mark.asyncio
-async def test_redirect_by_short_url(
-    async_client: AsyncClient,
-):
-    payload = {
-        "original_url": "https://www.google.com/",
-    }
-    response = await async_client.post(
-        "/api/v1/url/",
-        json=payload,
-    )
-    assert response.status_code == 201
-
-    short_url_id = 1
-    response = await async_client.get(
-        f"/api/v1/url/{short_url_id}/",
-    )
-    assert response.status_code == 307
-    assert response.headers['Location'] == payload['original_url']
-
